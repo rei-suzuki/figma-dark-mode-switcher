78f4203aa6097ab06d53d0c714cdac5cee3f970c
rei-suzuki <rei-suzuki@tokubai.co.jp>
2020-04-20 21:03:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![eyecatch](/assets/eyecatch.png')
+
+# Dark Mode Switcher
+Provides that can toggle light/dark color.
+
+You can specify your favor ColorStyle which separated by “/” and contains “Dark"("dark") or “Light”("light") key.
+Exmaple: “Background/Dark/Primary” will change to “Background/Light/Primary”.
+
+## Usage
+Select Frame, then choose 'Dark Mode Switcher'
+ → 'Dark mode' / 'Light mode'.
---
+++ code.js
@@ -0,0 +1,70 @@
+const localStyles = figma.getLocalPaintStyles();
+const lightStyleMap = {
+    'dark': 'light',
+    'Dark': 'Light',
+};
+const darkStyleMap = {
+    'light': 'dark',
+    'Light': 'Dark',
+};
+let mode = undefined;
+function main() {
+    if (figma.command == 'light') {
+        mode = 'Light';
+    }
+    else if (figma.command == 'dark') {
+        mode = 'Dark';
+    }
+    else {
+        figma.closePlugin();
+    }
+    try {
+        for (let i = 0; i < figma.currentPage.selection.length; i++) {
+            replaceNodes([figma.currentPage.selection[i]]);
+        }
+    }
+    catch (e) {
+        const error = e.toString();
+        return figma.closePlugin(error);
+    }
+    figma.closePlugin();
+}
+function replaceNodes(nodes) {
+    for (const node of nodes) {
+        const name = getPaintStyleNameByNode(node.fillStyleId);
+        if (name != null) {
+            const replacedColorStyleName = changeReplaceColorStyleName(name);
+            const replacedFillStyleId = getFillStyleIdByName(replacedColorStyleName);
+            node.fillStyleId = replacedFillStyleId;
+        }
+        if (node.type === 'COMPONENT' || node.type === 'INSTANCE' || node.type === 'FRAME' || node.type === 'GROUP' || node.type === 'PAGE') {
+            replaceNodes(node.children);
+        }
+    }
+}
+function getPaintStyleNameByNode(currentFillStyleId) {
+    const style = localStyles.find(style => style.id == currentFillStyleId);
+    return (style != undefined) ? style.name : null;
+}
+function changeReplaceColorStyleName(paintStyleName) {
+    const splitPaintStyleName = paintStyleName.split('/');
+    const replacedNodePaintStyleName = [];
+    for (let i = 0; i < splitPaintStyleName.length; i++) {
+        let name = splitPaintStyleName[i];
+        if (mode == 'Light' && lightStyleMap[name] != undefined) {
+            replacedNodePaintStyleName.push(lightStyleMap[name]);
+        }
+        else if (mode == 'Dark' && darkStyleMap[name] != undefined) {
+            replacedNodePaintStyleName.push(darkStyleMap[name]);
+        }
+        else {
+            replacedNodePaintStyleName.push(name);
+        }
+    }
+    return replacedNodePaintStyleName.join('/');
+}
+function getFillStyleIdByName(replacedColorStyleName) {
+    const style = localStyles.find(style => style.name == replacedColorStyleName);
+    return (style != undefined) ? style.id : null;
+}
+main();
